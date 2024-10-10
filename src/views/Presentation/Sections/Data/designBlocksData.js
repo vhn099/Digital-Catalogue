354e898efff2345e173051c554c6cbbe7de83258
Nhut Vo <nhut.vo@clv.local>
2024-10-10 18:02:21 +0700

    qua troi met

--- src/views/Presentation/Sections/Data/designBlocksData.js
+++ src/views/Presentation/Sections/Data/designBlocksData.js
@@ -31,282 +31,136 @@ import imgDropdowns from "@/assets/img/dropdowns.jpg";
 
 export default [
   {
-    heading: "Design Blocks",
+    heading: "Alcoholic Beverages",
     description:
-      "A selection of 45 page sections that fit perfectly in any combination",
+      "These drinks contain ethanol (alcohol) and can be further subdivided",
     items: [
       {
         image: `${imagesPrefix}/headers.jpg`,
-        title: "Page Headers",
-        subtitle: "10 Examples",
+        title: "Beer",
+        subtitle: "10 Items",
         route: "page-headers",
         pro: false
       },
       {
         image: imgFeatures,
-        title: "Features",
-        subtitle: "14 Examples",
+        title: "Wine",
+        subtitle: "14 Items",
         route: "page-features",
         pro: false
       },
       {
         image: imgPricing,
-        title: "Pricing",
-        subtitle: "8 Examples",
+        title: "Spirits/Liquor",
+        subtitle: "8 Items",
         route: "presentation",
-        pro: true
+        pro: false
       },
       {
         image: `${imagesPrefix}/faq.jpg`,
-        title: "FAQ",
+        title: "Cocktails",
         subtitle: "1 Example",
         route: "presentation",
-        pro: true
-      },
-      {
-        image: imgBlogPosts,
-        title: "Blog Posts",
-        subtitle: "11 Examples",
-        route: "presentation",
-        pro: true
-      },
-      {
-        image: imgTestimonials,
-        title: "Testimonials",
-        subtitle: "11 Examples",
-        route: "presentation",
-        pro: true
-      },
-      {
-        image: imgTeam,
-        title: "Teams",
-        subtitle: "6 Examples",
-        route: "presentation",
-        pro: true
-      },
-      {
-        image: imgStat,
-        title: "Stats",
-        subtitle: "3 Examples",
-        route: "presentation",
-        pro: true
-      },
-      {
-        image: `${imagesPrefix}/call-to-action.jpg`,
-        title: "Call to Actions",
-        subtitle: "8 Examples",
-        route: "presentation",
-        pro: true
-      },
-      {
-        image: `${imagesPrefix}/projects.jpg`,
-        title: "Applications",
-        subtitle: "6 Examples",
-        route: "presentation",
-        pro: true
-      },
-      {
-        image: `${imagesPrefix}/logo-area.jpg`,
-        title: "Logo Areas",
-        subtitle: "4 Examples",
-        route: "presentation",
-        pro: true
-      },
-      {
-        image: `${imagesPrefix}/footers.jpg`,
-        title: "Footers",
-        subtitle: "10 Examples",
-        route: "presentation",
-        pro: true
-      },
-      {
-        image: `${imagesPrefix}/general-cards.jpg`,
-        title: "General Cards",
-        subtitle: "9 Examples",
-        route: "presentation",
-        pro: true
-      },
-      {
-        image: imgContent,
-        title: "Content Sections",
-        subtitle: "8 Examples",
-        route: "presentation",
-        pro: true
+        pro: false
       }
     ]
   },
   {
-    heading: "Navigation",
-    description: "30+ components that will help go through the pages",
+    heading: "Hot Beverages",
+    description: "30+ Drinks typically served hot",
     items: [
       {
         image: `${imagesPrefix}/navbars.jpg`,
-        title: "Navbars",
-        subtitle: "4 Examples",
+        title: "Coffee",
+        subtitle: "4 Items",
         route: "navigation-navbars",
         pro: false
       },
       {
         image: `${imagesPrefix}/nav-tabs.jpg`,
-        title: "Nav Tabs",
+        title: "Tea",
         subtitle: "2 Nav Tabs",
         route: "navigation-navtabs",
         pro: false
       },
       {
         image: imgPagination,
-        title: "Pagination",
-        subtitle: "3 Examples",
+        title: "Hot Chocolate",
+        subtitle: "3 Items",
         route: "navigation-pagination",
         pro: false
       }
     ]
   },
   {
-    heading: "Input Areas",
+    heading: "Functional Drinks",
     description:
-      "50+ elements that you need for text manipulation and insertion",
+      "Beverages consumed for specific health benefits or energy",
     items: [
       {
         image: `${imagesPrefix}/newsletters.jpg`,
-        title: "Newsletters",
-        subtitle: "6 Examples",
+        title: "Energy Drinks",
+        subtitle: "6 Items",
         route: "presentation",
-        pro: true
+        pro: false
       },
       {
         image: `${imagesPrefix}/contact-sections.jpg`,
-        title: "Contact Sections",
-        subtitle: "8 Examples",
+        title: "Sports Drinks",
+        subtitle: "8 Items",
         route: "presentation",
-        pro: true
+        pro: false
       },
       {
         image: `${imagesPrefix}/forms.jpg`,
-        title: "Forms",
-        subtitle: "3 Examples",
+        title: "Protein Shakes",
+        subtitle: "3 Items",
         route: "inputareas-forms",
         pro: false
       },
       {
         image: `${imagesPrefix}/inputs.jpg`,
-        title: "Inputs",
-        subtitle: "6 Examples",
+        title: "Infusions",
+        subtitle: "6 Items",
         route: "inputareas-inputs",
         pro: false
       }
     ]
   },
   {
-    heading: "Attention Catchers",
+    heading: "Fermented Beverages",
     description:
-      "20+ Fully coded components that popup from different places of the screen",
+      "20+ These are non-alcoholic drinks made through fermentation",
     items: [
       {
         image: imgAlert,
-        title: "Alerts",
-        subtitle: "4 Examples",
+        title: "Kombucha",
+        subtitle: "4 Items",
         route: "ac-alerts",
         pro: false
       },
       {
         image: `${imagesPrefix}/toasts.jpg`,
-        title: "Notifications",
-        subtitle: "3 Examples",
+        title: "Kefir",
+        subtitle: "3 Items",
         route: "presentation",
-        pro: true
+        pro: false
       },
       {
         image: imgPopover,
-        title: "Tooltips & Popovers",
-        subtitle: "2 Examples",
+        title: "Kvass",
+        subtitle: "2 Items",
         route: "ac-tooltips-popovers",
         pro: false
       },
       {
         image: imgModal,
-        title: "Modals",
-        subtitle: "5 Examples",
+        title: "Smoothies",
+        subtitle: "5 Items",
         route: "ac-modals",
         pro: false
       }
     ]
-  },
-  {
-    heading: "Elements",
-    description:
-      "80+ carefully crafted small elements that come with multiple colors and shapes",
-    items: [
-      {
-        image: `${imagesPrefix}/buttons.jpg`,
-        title: "Buttons",
-        subtitle: "6 Examples",
-        route: "el-buttons",
-        pro: false
-      },
-      {
-        image: `${imagesPrefix}/avatars.jpg`,
-        title: "Avatars",
-        subtitle: "2 Examples",
-        route: "el-avatars",
-        pro: false
-      },
-      {
-        image: imgDropdowns,
-        title: "Dropdowns",
-        subtitle: "2 Examples",
-        route: "el-dropdowns",
-        pro: false
-      },
-      {
-        image: `${imagesPrefix}/switch.jpg`,
-        title: "Toggles",
-        subtitle: "2 Examples",
-        route: "el-toggles",
-        pro: false
-      },
-      {
-        image: `${imagesPrefix}/social-buttons.jpg`,
-        title: "Social Buttons",
-        subtitle: "2 Examples",
-        route: "presentation",
-        pro: true
-      },
-      {
-        image: `${imagesPrefix}/breadcrumbs.jpg`,
-        title: "Breadcrumbs",
-        subtitle: "1 Example",
-        route: "el-breadcrumbs",
-        pro: false
-      },
-      {
-        image: `${imagesPrefix}/badges.jpg`,
-        title: "Badges",
-        subtitle: "3 Examples",
-        route: "el-badges",
-        pro: false
-      },
-      {
-        image: `${imagesPrefix}/progress.jpg`,
-        title: "Progress Bars",
-        subtitle: "4 Examples",
-        route: "el-progress-bars",
-        pro: false
-      },
-      {
-        image: `${imagesPrefix}/tables.jpg`,
-        title: "Tables",
-        subtitle: "3 Examples",
-        route: "presentation",
-        pro: true
-      },
-      {
-        image: `${imagesPrefix}/typography.jpg`,
-        title: "Typography",
-        subtitle: "2 Examples",
-        route: "el-typography",
-        pro: false
-      }
-    ]
   }
+  
 ];
