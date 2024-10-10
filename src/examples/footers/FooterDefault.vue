3bd398de564127e3d9068b6129036d70364a33ea
Nhut Vo <nhut.vo@clv.local>
2024-10-10 18:14:35 +0700

    homepage

--- src/examples/footers/FooterDefault.vue
+++ src/examples/footers/FooterDefault.vue
@@ -123,62 +123,63 @@ defineProps({
 });
 </script>
 <template>
-  <footer class="footer pt-5 mt-5">
+  <footer class="footer pt-5 mt-5 pb-2 mb-5">
     <div class="container">
-      <div class="row">
-        <div class="col-md-3 mb-4 ms-auto">
-          <div>
-            <a :href="brand.route">
-              <img :src="brand.logo" class="mb-3 footer-logo" alt="main_logo" />
-            </a>
-            <h6 class="font-weight-bolder mb-4">{{ brand.name }}</h6>
-          </div>
-          <div>
-            <ul class="d-flex flex-row ms-n3 nav">
-              <li
-                class="nav-item"
-                v-for="{ icon, link } of socials"
-                :key="link"
+      <div class="row align-items-center justify-content-lg-between">
+            <div class="col-12 col-md-4 my-auto">
+              <div
+                class="copyright text-center text-sm text-lg-start"
               >
-                <a
-                  class="nav-link pe-1"
-                  :href="link"
-                  target="_blank"
-                  v-html="icon"
-                >
-                </a>
-              </li>
-            </ul>
+                Power By Adm GROUP
+              </div>
+            </div>
+            <div class="col-12 col-md-8">
+              <ul
+                class="nav nav-footer justify-content-center justify-content-lg-end"
+              >
+                <li class="nav-item">
+                  <a
+                    href="https://www.admgroup.com/cookie-policy"
+                    class="nav-link"
+                    target="_blank"
+                    >Cookie Policy</a
+                  >
+                </li>
+                <li class="nav-item">
+                  <a
+                    href="https://www.admgroup.com/cookie-policy"
+                    class="nav-link"
+                    target="_blank"
+                    >Accessibility Statement</a
+                  >
+                </li>
+                <li class="nav-item">
+                  <a
+                    href="https://www.admgroup.com/website-terms-of-use"
+                    class="nav-link"
+                    target="_blank"
+                    >Terms of Use</a
+                  >
+                </li>
+                <li class="nav-item">
+                  <a
+                    href="https://www.admgroup.com/website-privacy-policy"
+                    class="nav-link pe-0"
+                    target="_blank"
+                    >Privacy Policy</a
+                  >
+                </li>
+                <li class="nav-item">
+                  <a
+                    href="https://www.admgroup.com/data-security-policy"
+                    class="nav-link pe-0"
+                    target="_blank"
+                    >Data Security Policy</a
+                  >
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-        <div
-          class="col-md-2 col-sm-6 col-6 mb-4"
-          v-for="{ name, items } of menus"
-          :key="name"
-        >
-          <h6 class="text-sm">{{ name }}</h6>
-          <ul class="flex-column ms-n3 nav">
-            <li class="nav-item" v-for="item of items" :key="item.name">
-              <a class="nav-link" :href="item.href" target="_blank">
-                {{ item.name }}
-              </a>
-            </li>
-          </ul>
-        </div>
-
-        <div class="col-12">
-          <div class="text-center">
-            <p class="text-dark my-4 text-sm font-weight-normal">
-              All rights reserved. Copyright ©
-              {{ new Date().getFullYear() }}
-              Material Kit by
-              <a href="https://www.creative-tim.com" target="_blank"
-                >Creative Tim</a
-              >.
-            </p>
-          </div>
-        </div>
-      </div>
     </div>
   </footer>
 </template>
