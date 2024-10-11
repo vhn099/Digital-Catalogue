b3fa42e8b27d5232224852f15b909fcf2feaec85
huynh-tuan-dat <dat.huynhtuan.tpv@one-line.com>
2024-10-11 16:10:28 +0700

    Change login default layout

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -30,7 +30,7 @@ onMounted(() => {
           <div class="col-lg-4 col-md-8 col-12 mx-auto">
             <div class="card z-index-0 fadeIn3 fadeInBottom">
               <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-                <div class="bg-gradient-primary-blue shadow-success border-radius-lg py-3 pe-1" style="background-color: aqua !important;">
+                <div class="bg-gradient-success shadow-success border-radius-lg py-3 pe-1" style="background-color: aqua !important;">
                   <h4 class="text-white font-weight-bolder text-center mt-2 mb-0" >
                     Sign in
                   </h4>
