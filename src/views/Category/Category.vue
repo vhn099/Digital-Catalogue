b3994577c4bb79f213787d2d7cabcef6fb8fea72
Nhut Vo <vhn099@gmail.com>
2024-10-19 15:14:42 +0700

    change name deckcard

--- src/views/Category/Category.vue
+++ src/views/Category/Category.vue
@@ -1,11 +1,51 @@
 <script setup>
+import InputText from 'primevue/inputtext';
+import FloatLabel from 'primevue/floatlabel';
+import Button from 'primevue/button';
+
+
+// components
 import DockItem from '../../components/Dock.vue';
+import HeaderItem from '../../components/Header.vue';
+import CarouselItem from '../../components/Carousel.vue'
+import CateHorizonItem from '../../components/CateHorizon.vue'
+import SectionItem from '../../components/Section.vue'
+import CardItem from '../../components/DeckCard.vue'
+import { ref } from 'vue';
+const value2 = ref(null);
+
+
+function toggleDarkMode() {
+    document.documentElement.classList.toggle('my-app-dark');
+}
 </script>
 
 <template>
-    <DockItem></DockItem>
-    <h1>CATEGORY</h1>
-</template>
+  <DockItem></DockItem>
+  <CarouselItem></CarouselItem>
 
-<style scoped>
-</style>
+  <div class="flex">
+    <div class="col-1">
+
+    </div>
+    <div class="col-11">
+
+      <SectionItem></SectionItem>
+
+      <CateHorizonItem></CateHorizonItem>
+
+      <SectionItem></SectionItem>
+      <div class="flex">
+        
+      </div>
+      <div class="flex">
+        <CardItem></CardItem>
+        <CardItem></CardItem>
+        <CardItem></CardItem>
+      </div>
+    </div>
+
+
+  </div>
+
+</template>
--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -10,7 +10,7 @@ import HeaderItem from '../../components/Header.vue';
 import CarouselItem from '../../components/Carousel.vue'
 import CateHorizonItem from '../../components/CateHorizon.vue'
 import SectionItem from '../../components/Section.vue'
-import CardItem from '../../components/Card.vue'
+import CardItem from '../../components/DeckCard.vue'
 import { ref } from 'vue';
 const value2 = ref(null);
 
