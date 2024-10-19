10ac2c98b5fe02f3fdf52c78ca019b85d69c9b6f
quang369 <tvquang0903@gmail.com>
2024-10-19 14:21:40 +0700

    change Contactus script

--- src/views/ContactUs/ContactView.vue
+++ src/views/ContactUs/ContactView.vue
@@ -17,8 +17,6 @@ const lastName = ref(null);
 const userEmail = ref(null);
 const message = ref(null);
 const emails = ref([]);
-const isSuccess = ref(false);
-
 const bodyReq = ref({
   to: [],
   message: {
@@ -29,6 +27,21 @@ const bodyReq = ref({
     email: '',
   },
 });
+const isSuccess = ref(false);
+const sectionIcon = "src/assets/img/homepage/support.png";
+const sectionText = "Contact us";
+const line4 = {
+  title: 'Selected',
+  deck: 'Glassware Design'
+};
+
+const messagePageIconCSS = {
+  fontSize: "62px",
+  color: "#58DA67",
+  fontWeight: "bold"
+};
+
+const messagePageBody = 'contact';
 
 function toggleDarkMode() {
   document.documentElement.classList.toggle('my-app-dark');
@@ -65,28 +78,13 @@ async function submitForm() {
     bodyReq.value.message.firstName = '';
     bodyReq.value.message.lastName = '';
     bodyReq.value.message.email = '';
-    // router.push({name: 'home'});
+    //show SuccessPage
+    isSuccess.value = true;
   } catch (error) {
     console.error('Error adding feedback: ', error);
     alert('Error sending feedback: ' + error.message);
   }
 }
-
-const sectionIcon = "src/assets/img/homepage/support.png";
-const sectionText = "Contact us";
-const line4 = {
-  title: 'Selected',
-  deck: 'Glassware Design'
-}
-
-const messagePageIconCSS = {
-  fontSize: "62px",
-  color: "#58DA67",
-  fontWeight: "bold"
-};
-
-const messagePageBody = 'contact';
-
 </script>
 
 <template>
@@ -142,9 +140,6 @@ const messagePageBody = 'contact';
             <div class="flex">
               <div class="flex-1 p-2">
                 <Button label="Submit" :fluid="true" @click="submitForm()" raised />
-                <!-- <RouterLink :to="{ name: thanks }">
-                  <span>Test route</span>
-                </RouterLink> -->
               </div>
               <div class="flex-1 p-2">
 
