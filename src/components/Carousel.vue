e4e74d508866ba4efe52cffa925cc23ee51dbd54
Nhut Vo <vhn099@gmail.com>
2024-10-19 18:05:11 +0700

    homepage

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,14 +1,21 @@
 <template>
 
-    <Galleria :value="images" :numVisible="5" containerStyle="max-width: 100%; height: 350px" :showThumbnails="false"
+    <Galleria :value="images" :numVisible="5" containerStyle="max-width: 100%; height: 425px" :showThumbnails="false"
         :showIndicators="true" :circular="true" :autoPlay="true" :transitionInterval="3000"
         :showIndicatorsOnItem="inside" :indicatorsPosition="position">
         <template #item="slotProps">
-            <div class="slideshow-canva">
-            <div class="slideshow-text"></div>
-            <div class="slideshow-image"></div>
 
+            <div class="slideshow-canva">
+                <div class="slideshow-text">
+                    <span class="line-1">New Collection Just In!</span>
+                    <span class="line-2">Get Yours Now with an Exclusive Discount!</span>
+                    <Button label="View Deck" />
+                </div>
+                <div class="slideshow-image">
+                    <img height="100%" fill="none" :src="slotProps.item.itemImageSrc" />
+                </div>
             </div>
+
             <!-- <img :src="slotProps.item.itemImageSrc" :alt="slotProps.item.alt" style="width: 100%; display: block" /> -->
         </template>
         <!-- <template #caption="slotProps">
@@ -21,29 +28,23 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import Galleria from 'primevue/galleria';
+import Button from 'primevue/button';
 const position = ref('bottom');
 const images = [
     {
-        itemImageSrc: 'https://primefaces.org/cdn/primevue/images/galleria/galleria1.jpg',
-        thumbnailImageSrc: 'https://primefaces.org/cdn/primevue/images/galleria/galleria1s.jpg',
+        itemImageSrc: '../src/assets/img/carousel/CAP.png',
         alt: 'Description for Image 1',
         title: 'Title 1'
     }, {
-        itemImageSrc: 'https://primefaces.org/cdn/primevue/images/galleria/galleria2.jpg',
-        thumbnailImageSrc: 'https://primefaces.org/cdn/primevue/images/galleria/galleria1s.jpg',
+        itemImageSrc: '../src/assets/img/carousel/CARLTON DRAUGHT _ESKY - 2 - CUB-CND-1004 1.png',
         alt: 'Description for Image 1',
         title: 'Title 2'
     }, {
-        itemImageSrc: 'https://primefaces.org/cdn/primevue/images/galleria/galleria3.jpg',
-        thumbnailImageSrc: 'https://primefaces.org/cdn/primevue/images/galleria/galleria1s.jpg',
+        itemImageSrc: '../src/assets/img/carousel/Pepsi_Shirt.png',
         alt: 'Description for Image 1',
-        title: 'Title 3'
-    }, {
-        itemImageSrc: 'https://primefaces.org/cdn/primevue/images/galleria/galleria4.jpg',
-        thumbnailImageSrc: 'https://primefaces.org/cdn/primevue/images/galleria/galleria1s.jpg',
-        alt: 'Description for Image 1',
-        title: 'Title 4'
-    },
+        title: 'Title 3',
+        backgroundColor: ''
+    }
 ];
 const inside = ref(true);
 
@@ -55,11 +56,11 @@ const inside = ref(true);
 }
 
 :deep(.p-galleria-content) {
-    height: 350px;
+    height: 425px;
 }
 
 :deep(.p-galleria-items-container) {
-    height: 350px;
+    height: 425px;
 }
 
 :deep(.p-galleria-caption) {
@@ -75,5 +76,50 @@ const inside = ref(true);
     width: 100%;
     height: 100%;
     background-color: aliceblue;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    background: linear-gradient(90deg, #FC3134 0%, #7B0E0F 100%);
+}
+
+.slideshow-text {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+
+.slideshow-text .line-1 {
+    font-size: 58px;
+    font-weight: 700;
+    line-height: 87px;
+    text-align: left;
+    color: white;
+}
+
+.slideshow-text .line-2 {
+    font-size: 32px;
+    font-weight: 500;
+    line-height: 48px;
+    text-align: left;
+    color: white;
+}
+
+.slideshow-text button {
+    border: 5px solid #FFFFFF;
+    width: 30%;
+}
+
+.slideshow-text button::hover {
+    border: 5px solid #FFFFFF;
+    width: 30%;
+}
+
+:deep(.p-button:not(:disabled):hover) {
+    border: 5px solid #FFFFFF;
+    width: 30%;
+}
+
+.slideshow-image {
+    justify-content: flex-end;
 }
 </style>
