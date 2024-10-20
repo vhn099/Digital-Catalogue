741c6b8182e4726215a098b593245484aca5f04f
Nhut Vo <vhn099@gmail.com>
2024-10-20 17:48:01 +0700

    pdf

--- src/views/DeckDetail/DeckDetail.vue
+++ src/views/DeckDetail/DeckDetail.vue
@@ -3,7 +3,23 @@
 </script>
 
 <template>
-    Deck Detail
+    <!-- Deck Detail -->
+    <embed
+    src="http://infolab.stanford.edu/pub/papers/google.pdf#toolbar=0&navpanes=0&scrollbar=0"
+    type="application/pdf"
+    frameBorder="0"
+    scrolling="auto"
+    height="100%"
+    width="100%"
+></embed>
+
+<iframe
+    src="http://infolab.stanford.edu/pub/papers/google.pdf#toolbar=0&navpanes=0&scrollbar=0"
+    frameBorder="0"
+    scrolling="auto"
+    height="100%"
+    width="100%"
+></iframe>
 </template>
 
 <style scoped>
