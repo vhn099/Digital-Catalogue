ddb6a34ffd2d9d2ae513fec7c383ee4e368877b7
quang369 <tvquang0903@gmail.com>
2024-10-25 18:01:15 +0700

    delete Fav

--- src/views/MyFavorite/MyFavorite.vue
+++ src/views/MyFavorite/MyFavorite.vue
@@ -4,20 +4,25 @@ import DockItem from '../../components/Dock.vue';
 import SectionItem from '../../components/Section.vue'
 import MyFavCardItem from '../../components/MyFavCard.vue';
 import FavoriteBlackIcon from '@/assets/img/icon/favorite_black.png';
-import { query, collection, getDocs, where } from "firebase/firestore";
-import { onMounted, ref, watch } from 'vue';
+import { query, collection, getDocs, where, getDoc, doc, deleteDoc } from "firebase/firestore";
+import { onMounted, ref } from 'vue';
 import { db } from '@/main';
-import { useAppStore } from '@/stores';
 import { getAuth } from 'firebase/auth';
-import { getDoc } from 'firebase/firestore';
-import { doc } from 'firebase/firestore';
-import { getFirestore } from 'firebase/firestore';
 const sectionIcon = FavoriteBlackIcon;
 const sectionText = "My Favorite";
 const favorites = ref();
 const email = ref('');
 const decks = ref();
-const isDeleteFav = ref(false);
+
+const deleteFav = async (id) => {
+    const documentRef = doc(db, 'favorite', id);
+    try {
+        await deleteDoc(documentRef);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+    decks.value = decks.value.filter((deck) => deck.fav_id != id);
+};
 
 const getFavorites = async () => {
     const favoriteList = [];
@@ -28,6 +33,7 @@ const getFavorites = async () => {
         querySnapshot.forEach(obj => {
             const data = obj.data();
             const object = {
+                favID: obj.id,
                 userID: data.userID || '',
                 deckID: data.deckID || '',
             };
@@ -47,6 +53,7 @@ const getDeckInfo = async (arr) => {
         const docSnap = await getDoc(docRef);
         if (docSnap.exists()) {
             const object = {
+                fav_id: arr[i].favID,
                 deck_image: docSnap.data().deck_highlight || '',
                 deck_name: docSnap.data().title || '',
                 deck_description: docSnap.data().detail_description || '',
@@ -54,15 +61,15 @@ const getDeckInfo = async (arr) => {
             };
             //console.log(object);
             deckList.push(object);
-        } 
+        }
     }
     return deckList;
 };
+
 onMounted(async () => {
     email.value = getAuth().currentUser.email;
     favorites.value = await getFavorites();
     decks.value = await getDeckInfo(favorites.value);
-
 });
 </script>
 
@@ -80,7 +87,9 @@ onMounted(async () => {
 
             <div class="fav-canva">
                 <div v-for="fav in decks" :key="fav">
-                    <MyFavCardItem :tag_arr="fav.deck_tag" :deck_img="fav.deck_image" :title="fav.deck_name" :description="fav.deck_description"></MyFavCardItem>
+                    <MyFavCardItem :fav_id="fav.fav_id" :tag_arr="fav.deck_tag" :deck_img="fav.deck_image"
+                        :title="fav.deck_name" :description="fav.deck_description" @callFunction="deleteFav">
+                    </MyFavCardItem>
                 </div>
                 <MyFavCardItem :isnew="true"></MyFavCardItem>
             </div>
