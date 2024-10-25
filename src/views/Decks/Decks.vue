70d1b53b697e6177bb868dc06d9fafdb854518e9
Nhut Vo <vhn099@gmail.com>
2024-10-25 14:43:39 +0700

    my profile vip pro

--- src/views/Decks/Decks.vue
+++ src/views/Decks/Decks.vue
@@ -5,12 +5,78 @@ import SectionItem from '../../components/Section.vue';
 import DeckItem from '../../components/DeckCard.vue';
 import SupplyChainIcon from '@/assets/img/icon/supply-chain.png';
 
+import Select from 'primevue/select';
+import Button from 'primevue/button';
+
+import Popover from 'primevue/popover';
+
+
 import { ref } from 'vue';
 const sectionIcon = SupplyChainIcon;
 const sectionText = "Decks";
+
+const op = ref();
+const members = ref([
+  { name: 'Amy Elsner', image: 'amyelsner.png', email: 'amy@email.com', role: 'Owner' },
+  { name: 'Bernardo Dominic', image: 'bernardodominic.png', email: 'bernardo@email.com', role: 'Editor' },
+  { name: 'Ioni Bowcher', image: 'ionibowcher.png', email: 'ioni@email.com', role: 'Viewer' }
+]);
+
+const toggle = (event) => {
+  op.value.toggle(event);
+}
+
+
+const selectedCity = ref();
+const cities = ref([
+  { name: 'A-z', code: 'NY' },
+  { name: 'z-A', code: 'RM' },
+  { name: 'Latest Update', code: 'LDN' },
+  { name: 'Most Favorited', code: 'IST' },
+]);
+
 </script>
 
 <template>
+
+  <Popover ref="op">
+    <div class="flex flex-col gap-4 w-[25rem]">
+      <div>
+        <span class="font-medium block mb-2">Share this document</span>
+        <InputGroup>
+          <InputText value="https://primevue.org/12323ff26t2g243g423g234gg52hy25XADXAG3" readonly class="w-[25rem]">
+          </InputText>
+          <InputGroupAddon>
+            <i class="pi pi-copy"></i>
+          </InputGroupAddon>
+        </InputGroup>
+      </div>
+      <div>
+        <span class="font-medium block mb-2">Invite Member</span>
+        <InputGroup>
+          <InputText disabled />
+          <Button label="Invite" icon="pi pi-users"></Button>
+        </InputGroup>
+      </div>
+      <div>
+        <span class="font-medium block mb-2">Team Members</span>
+        <ul class="list-none p-0 m-0 flex flex-col gap-4">
+          <li v-for="member in members" :key="member.name" class="flex items-center gap-2">
+            <img :src="`https://primefaces.org/cdn/primevue/images/avatar/${member.image}`" style="width: 32px" />
+            <div>
+              <span class="font-medium">{{ member.name }}</span>
+              <div class="text-sm text-surface-500 dark:text-surface-400">{{ member.email }}</div>
+            </div>
+            <div class="flex items-center gap-2 text-surface-500 dark:text-surface-400 ml-auto text-sm">
+              <span>{{ member.role }}</span>
+              <i class="pi pi-angle-down"></i>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </Popover>
+
   <DockItem></DockItem>
 
   <div class="flex">
@@ -20,7 +86,14 @@ const sectionText = "Decks";
     <div class="col-11">
 
       <!-- <SectionItem></SectionItem> -->
-      <SectionItem :icon="sectionIcon" :icon_text="sectionText"></SectionItem>
+      <div class="deck-section-filter">
+        <SectionItem :icon="sectionIcon" :icon_text="sectionText"></SectionItem>
+        <div class="deck-filter gap-2">
+          <Select class="mt-1 pt-4" v-model="selectedCity" :options="cities" optionLabel="name"
+            placeholder="Sort By:" />
+          <Button class="mt-1 pt-4" type="button" icon="pi pi-sliders-h" @click="toggle" />
+        </div>
+      </div>
 
       <div class="card top-line">
         <DeckItem></DeckItem>
@@ -48,5 +121,9 @@ const sectionText = "Decks";
 </template>
 
 <style scoped>
-
+.deck-section-filter {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
 </style>
