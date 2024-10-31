0833b28d40d48686b1fc31471a5bad7785b8180d
huynh-tuan-dat <dat.huynhtuan.tpv@one-line.com>
2024-10-31 21:27:05 +0700

    Adjustment for UI blocked.vue

--- src/components/Blocked.vue
+++ src/components/Blocked.vue
@@ -1,11 +1,11 @@
 <script setup>
-
+    
 </script>
 
 <template>
-    <p class="page-message">Your account has been locked due to multiple failed login attempts.</p>
-    <p class="page-message">Please reset your password or contact support for assistance.</p>
-    <a href="#" class="contact-our-team">Contact our team</a>
+    <p class="page-message">Your account has been locked.</p>
+    <p class="page-message">Please contact our team for more assistance.</p>
+    <a href="mailto:asahicubau@admgroup.com" class="contact-our-team">Contact our team</a>
 </template>
 
 <style scoped>
